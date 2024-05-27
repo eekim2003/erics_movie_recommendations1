6dbd4a6f37898e375c7179d12bf14e20a25bae8a
Eric Kim <erickim@Erics-MBP-2.attlocal.net>
2024-05-27 15:18:20 -0400

    style6

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 def load_movies(file_path):
     with open(file_path, 'r') as file:
